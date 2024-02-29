4eeef1224a79f3907316c80b98ab92e5fea4b268
Anna Freiman <annafrei924@gmail.com>
2024-02-28 20:23:28 -0500

    added sand class and tests

---
+++ src/main/java/freiman/fallingsand/Sand.java
@@ -0,0 +1,40 @@
+package freiman.fallingsand;
+
+public class Sand {
+
+    private int [][] field = new int[3][3];
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                builder.append(field[y][x]);
+            }
+            builder.append("\n");
+        }
+
+        return builder.toString();
+    }
+
+    /**
+     * @return the value in the field
+     */
+    public int get(int x, int y) {
+        return field[y][x];
+    }
+
+    /**
+     *  Sets the value in the field to be 1
+     */
+    public void put(int x, int y) {
+        field[y][x] = 1;
+    }
+    public void fall(){
+        //moves all sand down one square
+
+
+    }
+
+
+}
