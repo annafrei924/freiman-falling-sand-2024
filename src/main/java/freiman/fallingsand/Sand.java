c67c70426b76c8ac250b85e6e1fa6c33c57eb221
Anna Freiman <annafrei924@gmail.com>
2024-02-28 23:23:49 -0500

    sand simulation first homework

--- src/main/java/freiman/fallingsand/Sand.java
+++ src/main/java/freiman/fallingsand/Sand.java
@@ -20,21 +20,37 @@ public class Sand {
     /**
      * @return the value in the field
      */
+
     public int get(int x, int y) {
+
         return field[y][x];
+
     }
 
     /**
      *  Sets the value in the field to be 1
      */
+
     public void put(int x, int y) {
+
         field[y][x] = 1;
+
     }
+
     public void fall(){
         //moves all sand down one square
+        //starting from second to last row; last row doens't matter bc can't move down
+        for (int y = field.length - 2; y >= 0 ; y--) {
 
+            for (int x = 0; x < 3; x++) {
+                // if there's sand here but not below
+                if (field[y][x] == 1 && field[y + 1][x] == 0) {
+                    field[y][x] = 0;
+                    field[y + 1][x] = 1;
+                }
+            }
+        }
 
     }
-
 
 }
--- src/test/java/freiman/fallingsand/SandTest.java
+++ src/test/java/freiman/fallingsand/SandTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SandTest
 {
+
     @Test
     public void string()  {
         //given
