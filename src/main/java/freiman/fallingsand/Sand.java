b423a136d5cbf6d6c21b14b40ef4c1c05f663fab
Anna Freiman <annafrei924@gmail.com>
2024-03-07 16:20:53 -0500

    inital push - not building

--- src/main/java/freiman/fallingsand/Sand.java
+++ src/main/java/freiman/fallingsand/Sand.java
@@ -4,23 +4,40 @@ import java.util.Random;
 
 public class Sand {
 
-    private final Random rand = new Random();
-    private int width;
-    private int height;
-    private int[][] field;
+    private final int[][] field;
+
+    private final Random random;
 
     public Sand(int width, int height) {
-        this.width = width;
-        this.height = height;
-        this.field = new int[height][width];
+        field = new int[height][width];
+        this.random = new Random();
     }
 
+    public Sand(int width, int height, Random random) {
+        field = new int[height][width];
+        this.random = random;
+    }
+
+    public static void main(String[] args) {
+        Sand sand = new Sand(50, 10);
+        sand.randomSand(10);
+        printGrid(sand.field);
+    }
+
+    public static void printGrid(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+    }
 
     public String toString() {
         StringBuilder builder = new StringBuilder();
 
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
+        for (int y = 0; y < field.length; y++) {
+            for (int x = 0; x < field[y].length; x++) {
                 builder.append(field[y][x]);
             }
             builder.append("\n");
@@ -30,53 +47,58 @@ public class Sand {
     }
 
     /**
-     * @return the value in the field
+     * @return the value in field
      */
-
     public int get(int x, int y) {
-
         return field[y][x];
-
     }
 
     /**
-     * Sets the value in the field to be 1
+     * Sets the value in field to be 1
      */
-
     public void put(int x, int y) {
-
         field[y][x] = 1;
-
     }
 
     public void fall() {
-        //moves all sand down one square
-        //starting from second to last row; last row doesn't matter bc can't move down
-        for (int y = height - 2; y >= 0; y--) {
-            for (int x = 0; x < width; x++) {
-
+        // moves all sand down one square
+        for (int y = field.length - 2; y >= 0; y--) {
+            for (int x = 0; x < field[0].length; x++) {
                 if (field[y][x] == 1) {
-
-                    // does the sand fall straight down
                     if (field[y + 1][x] == 0) {
+                        // does the sand fall straight down?
                         field[y][x] = 0;
                         field[y + 1][x] = 1;
+                        continue;
                     }
-                    //does the sand fall right
-                    else if (field[y + 1][x + 1] == 0) {
-                        field[y][x] = 0;
-                        field[y + 1][x + 1] = 1;
 
-                    }
-                    //does the dans fall left
-                    else if (field[y + 1][x - 1] == 0) {
-                        field[y][x] = 0;
-                        field[y + 1][x - 1] = 1;
+                    boolean rightFirst = random.nextBoolean();
+                    int direction1 = rightFirst ? +1 : -1;
+                    int direction2 = rightFirst ? -1 : +1;
 
+                    if (field[y + 1][x + direction1] == 0) {
+                        field[y][x] = 0;
+                        field[y + 1][x + direction1] = 1;
+                    } else if (field[y + 1][x + direction2] == 0) {
+                        field[y][x] = 0;
+                        field[y + 1][x + direction2] = 1;
                     }
                 }
             }
         }
     }
 
+    public void randomSand(int n) {
+        int x, y;
+        for (int i = 0; i < n; i++) {
+            do {
+                x = random.nextInt(field[0].length);
+                y = random.nextInt(field.length);
+                field[y][x] = 1;
+            } while (field[y][x] == 1);
+        }
+    }
+
 }
+
+
--- src/test/java/freiman/fallingsand/SandTest.java
+++ src/test/java/freiman/fallingsand/SandTest.java
@@ -2,96 +2,138 @@ package freiman.fallingsand;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 class SandTest {
 
-
     @Test
     public void string() {
-        //given
+        // given
         Sand sand = new Sand(3, 3);
-        //when
+
+        // when
         String actual = sand.toString();
-        //then
+
+        // then
         assertEquals("000\n000\n000\n", actual);
     }
 
     @Test
     public void put() {
-        //given
+        // given
         Sand sand = new Sand(3, 3);
-        //when
+
+        // when
         sand.put(1, 0);
-        //then
+
+        // then
         assertEquals("010\n000\n000\n", sand.toString());
     }
 
     @Test
     public void fall() {
-        //given
+        // given
         Sand sand = new Sand(3, 3);
         sand.put(1, 0);
 
-        //when
+        // when
         sand.fall();
 
-        //then
+        // then
         assertEquals("000\n010\n000\n", sand.toString());
     }
 
     @Test
     public void fallOnGround() {
-        //given
+        // given
         Sand sand = new Sand(3, 3);
         sand.put(1, 2);
 
-        //when
+        // when
         sand.fall();
 
-        //then
+        // then
         assertEquals("000\n000\n010\n", sand.toString());
     }
 
     @Test
     public void fallToTheRight() {
-        //given
+        // given
         Sand sand = new Sand(3, 3);
         sand.put(1, 1);
         sand.put(1, 2);
-        sand.put(0, 2); //left
+        sand.put(0, 2); // left
 
-        //when
+        // when
         sand.fall();
 
-        //then
+        // then
         assertEquals("000\n000\n111\n", sand.toString());
     }
 
     @Test
     public void fallToTheLeft() {
-        //given
+        // given
         Sand sand = new Sand(3, 3);
         sand.put(1, 1);
         sand.put(1, 2);
-        sand.put(2, 2); //left
+        sand.put(2, 2); // right
 
-        //when
+        // when
         sand.fall();
 
-        //then
+        // then
         assertEquals("000\n000\n111\n", sand.toString());
     }
 
     @Test
-    public void fallSimultaneously() {
+    public void fallRandomDirectionRight() {
+        // given
+        Random random = mock();
+        doReturn(true).when(random).nextBoolean();
+        Sand sand = new Sand(3, 3, random);
+        sand.put(1, 1);
+        sand.put(1, 2);
 
+        // when
+        sand.fall();
+
+        // then
+        assertEquals("000\n000\n011\n", sand.toString());
+    }
+
+    @Test
+    public void fallRandomDirectionLeft() {
+        // given
+        Random random = mock();
+        doReturn(false).when(random).nextBoolean();
+        Sand sand = new Sand(3, 3, random);
+        sand.put(1, 1);
+        sand.put(1, 2);
+
+        // when
+        sand.fall();
+
+        // then
+        assertEquals("000\n000\n110\n", sand.toString());
+    }
+
+    @Test
+    public void fallSimultaneously() {
+        // given
         Sand sand = new Sand(3, 3);
         sand.put(1, 0);
         sand.put(1, 1);
 
+        // when
         sand.fall();
 
+        // then
         assertEquals("000\n010\n010\n", sand.toString());
     }
+
 }
